3a793368284fda7b0e3dd64f31ed51ed7dd5b306
Larin Nikita <l_nikita2000@mail.ru>
2025-02-09 15:43:57 +0700

    switch pnpm runtime manager

--- src/app/(home)/client.tsx
+++ src/app/(home)/client.tsx
@@ -1,15 +1,5 @@
 'use client';
 
-import { trpc } from '@/trpc/client';
-
 export const PageClient = () => {
-    const [data] = trpc.hello.useSuspenseQuery({
-        text: '(╯°□°）╯︵ ┻━┻',
-    });
-
-    return (
-        <div>
-            <h1>Client component: {data.greeting}</h1>
-        </div>
-    );
+    return <div></div>;
 };
