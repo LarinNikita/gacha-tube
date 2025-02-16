0c0e2f6e98bdacc6739ab32c485e50eb891c5b03
Larin Nikita <l_nikita2000@mail.ru>
2025-02-16 14:06:03 +0700

    Add skeleton to videos-section

--- src/modules/studio/ui/sections/videos-section.tsx
+++ src/modules/studio/ui/sections/videos-section.tsx
@@ -15,6 +15,7 @@ import { VideoThumbnail } from '@/modules/videos/ui/components/video-thumbnail';
 
 import { trpc } from '@/trpc/client';
 
+import { Skeleton } from '@/components/ui/skeleton';
 import { InfiniteScroll } from '@/components/infinite-scroll';
 import {
     Table,
@@ -27,11 +28,72 @@ import {
 
 export const VideosSection = () => {
     return (
-        <Suspense fallback={<p>Loading...</p>}>
+        <Suspense fallback={<VideosSectionSkeleton />}>
             <ErrorBoundary fallback={<p>Something went wrong</p>}>
                 <VideosSectionSuspense />
             </ErrorBoundary>
         </Suspense>
+    );
+};
+
+const VideosSectionSkeleton = () => {
+    return (
+        <>
+            <div className="border-y">
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead className="w-[510px] pl-6">
+                                Video
+                            </TableHead>
+                            <TableHead>Visibility</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Date</TableHead>
+                            <TableHead className="text-right">Views</TableHead>
+                            <TableHead className="text-right">
+                                Comments
+                            </TableHead>
+                            <TableHead className="pr-6 text-right">
+                                Likes
+                            </TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {Array.from({ length: DEFAULT_LIMIT }).map((_, i) => (
+                            <TableRow key={i}>
+                                <TableCell className="pl-6">
+                                    <div className="flex items-center gap-4">
+                                        <Skeleton className="h-20 w-36" />
+                                        <div className="flex flex-col gap-2">
+                                            <Skeleton className="h-4 w-[100px]" />
+                                            <Skeleton className="h-3 w-[150px]" />
+                                        </div>
+                                    </div>
+                                </TableCell>
+                                <TableCell>
+                                    <Skeleton className="h-4 w-20" />
+                                </TableCell>
+                                <TableCell>
+                                    <Skeleton className="h-4 w-16" />
+                                </TableCell>
+                                <TableCell>
+                                    <Skeleton className="h-4 w-24" />
+                                </TableCell>
+                                <TableCell className="text-right">
+                                    <Skeleton className="ml-auto h-4 w-12" />
+                                </TableCell>
+                                <TableCell className="text-right">
+                                    <Skeleton className="ml-auto h-4 w-12" />
+                                </TableCell>
+                                <TableCell className="pr-6 text-right">
+                                    <Skeleton className="ml-auto h-4 w-12" />
+                                </TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </div>
+        </>
     );
 };
 
@@ -76,7 +138,7 @@ const VideosSectionSuspense = () => {
                                     legacyBehavior
                                 >
                                     <TableRow className="cursor-pointer">
-                                        <TableCell>
+                                        <TableCell className="pl-6">
                                             <div className="flex items-center gap-4">
                                                 <div className="relative aspect-video w-36 shrink-0">
                                                     <VideoThumbnail
@@ -129,9 +191,15 @@ const VideosSectionSuspense = () => {
                                                 'd MMM yyyy',
                                             )}
                                         </TableCell>
-                                        <TableCell>views</TableCell>
-                                        <TableCell>comments</TableCell>
-                                        <TableCell>likes</TableCell>
+                                        <TableCell className="text-right text-sm">
+                                            views
+                                        </TableCell>
+                                        <TableCell className="text-right text-sm">
+                                            comments
+                                        </TableCell>
+                                        <TableCell className="pr-6 text-right text-sm">
+                                            likes
+                                        </TableCell>
                                     </TableRow>
                                 </Link>
                             ))}
