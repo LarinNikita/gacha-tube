5c937ce70072cce14ca59bc25e454323c4edfed0
Larin Nikita <l_nikita2000@mail.ru>
2025-03-05 11:19:24 +0700

    Modify comments getMany procedure. Change prefetch to prefetchInfinite. Change suspense to  useSuspenseInfiniteQuery. Add infinite loading component. Add skeleton comments section. Prepare dropdown options. Add comments count.

--- src/modules/comments/ui/components/comment-item.tsx
+++ src/modules/comments/ui/components/comment-item.tsx
@@ -1,15 +1,45 @@
 import Link from 'next/link';
+import { toast } from 'sonner';
 import { formatDistanceToNow } from 'date-fns';
+import { useAuth, useClerk } from '@clerk/nextjs';
+import { MessageSquareIcon, MoreVerticalIcon, Trash2Icon } from 'lucide-react';
+
+import { trpc } from '@/trpc/client';
 
 import { CommentsGetManyOutput } from '../../types';
 
+import { Button } from '@/components/ui/button';
 import { UserAvatar } from '@/components/user-avatar';
+import {
+    DropdownMenu,
+    DropdownMenuContent,
+    DropdownMenuItem,
+    DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 
 interface CommentFormProps {
-    comment: CommentsGetManyOutput[number];
+    comment: CommentsGetManyOutput['items'][number];
 }
 
 export const CommentItem = ({ comment }: CommentFormProps) => {
+    const { userId } = useAuth();
+    const clerk = useClerk();
+
+    const utils = trpc.useUtils();
+    const remove = trpc.comments.remove.useMutation({
+        onSuccess: () => {
+            toast.success('Comment deleted');
+            utils.comments.getMany.invalidate({ videoId: comment.videoId });
+        },
+        onError: error => {
+            toast.error('Something went wrong');
+
+            if (error.data?.code === 'UNAUTHORIZED') {
+                clerk.openSignIn();
+            }
+        },
+    });
+
     return (
         <div>
             <div className="flex gap-4">
@@ -34,7 +64,31 @@ export const CommentItem = ({ comment }: CommentFormProps) => {
                         </div>
                     </Link>
                     <p className="text-sm">{comment.value}</p>
+                    {/* TODO: Add Reactions */}
                 </div>
+                <DropdownMenu>
+                    <DropdownMenuTrigger asChild>
+                        <Button variant="ghost" size="icon" className="size-8">
+                            <MoreVerticalIcon />
+                        </Button>
+                    </DropdownMenuTrigger>
+                    <DropdownMenuContent align="end">
+                        <DropdownMenuItem onClick={() => {}}>
+                            <MessageSquareIcon className="size-4" />
+                            Reply
+                        </DropdownMenuItem>
+                        {comment.user.clerkId === userId && (
+                            <DropdownMenuItem
+                                onClick={() =>
+                                    remove.mutate({ id: comment.id })
+                                }
+                            >
+                                <Trash2Icon className="size-4" />
+                                Delete
+                            </DropdownMenuItem>
+                        )}
+                    </DropdownMenuContent>
+                </DropdownMenu>
             </div>
         </div>
     );
